41e217902a5b19eb96fd92d10409cffd450a79ce
GabriellyS <santos.gabrielly14@gmail.com>
2026-02-16 10:32:29 -0300

    feat: add user repository

---
+++ src/main/java/com/example/user_crud_api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.user_crud_api.repository;
+
+import com.example.user_crud_api.model.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
+}
