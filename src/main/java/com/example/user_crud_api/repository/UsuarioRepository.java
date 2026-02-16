eae488e9686ab6fea516b128236d22c5d2234ccc
GabriellyS <santos.gabrielly14@gmail.com>
2026-02-16 11:10:21 -0300

    feat: add name filter to repository

--- src/main/java/com/example/user_crud_api/repository/UsuarioRepository.java
+++ src/main/java/com/example/user_crud_api/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.example.user_crud_api.repository;
 
 import com.example.user_crud_api.model.UsuarioModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
+    public Page<UsuarioModel> findByNameContainingIgnoreCase (String nome, Pageable pageable);
 
 }
