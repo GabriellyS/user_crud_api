3c3925d9bf6d15ede0811545f1952aff760c2c26
GabriellyS <santos.gabrielly14@gmail.com>
2026-02-16 10:01:27 -0300

    feat: create user entity

---
+++ src/main/java/com/example/user_crud_api/model/UsuarioModel.java
@@ -0,0 +1,41 @@
+package com.example.user_crud_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "usuarios")
+public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String senha;
+
+    @Column(unique = true)
+    private String email;
+
+    public UsuarioModel(String name, String senha, String email) {
+        this.name = name;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
