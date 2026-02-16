5a251c830e90b57583324480faf8972c69c1a7ca
GabriellyS <santos.gabrielly14@gmail.com>
2026-02-16 09:02:45 -0300

    chore: initial project setup

---
+++ src/test/java/com/example/user_crud_api/UserCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
